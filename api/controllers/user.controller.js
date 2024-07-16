54fcc691f7aecc855516c6b14fb965954177c767
atharvgk <atharv.katyarmal@gmail.com>
2024-07-16 11:55:50 +0530

    add delete user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,3 +53,15 @@ export const updateUser = async (req, res, next) => {
     }
   }
 };
+
+export const deleteUser=async (req,res,next)=>{
+  if (req.user.id!==req.params.userId) {
+    return next(errorHandler(403,"this user cant be deleted"))
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("user has been deleted")
+  } catch (error) {
+    next(error)
+  }
+}
